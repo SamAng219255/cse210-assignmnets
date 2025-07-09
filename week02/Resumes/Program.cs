da681079b563ac85aa5f75920be4688ca267809c
SamAng219255 <SamAng033146085@gmail.com>
2025-07-09 12:44:51 -0700

    Completed week 02 learning activity: Abstraction.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2026;
+        job1._endYear = 2036;
+        Job job2 = new Job();
+        job2._jobTitle = "Programmer";
+        job2._company = "Perfectly Generic Team";
+        job2._startYear = 2019;
+        job2._endYear = 2020;
+
+        Resume resume = new Resume();
+        resume._name = "Sam Anguiano";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
