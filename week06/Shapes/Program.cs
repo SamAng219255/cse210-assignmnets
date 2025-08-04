25565a36f676428f8b12abde0508287df86cfab1
SamAng219255 <SamAng033146085@gmail.com>
2025-08-04 10:03:43 -0700

    Comleted week 06 learning activity.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>{
+            new Square("red", 3.0),
+            new Rectangle("green", 5.0, 6.0),
+            new Circle("blue", 2.0)
+        };
+
+        foreach(Shape shape in shapes) {
+            Console.WriteLine($"Color: {shape.GetColor()}\nArea: {shape.GetArea()}\n");
+        }
     }
 }
